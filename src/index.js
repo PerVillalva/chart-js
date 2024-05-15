7938b435e97172002a309527314844829abb0b68
pvillalva <pervillalva@gmail.com>
2024-05-15 14:04:36 -0300

    test-wp-2

--- src/index.js
+++ src/index.js
@@ -1,49 +1,53 @@
 import ApexCharts from 'apexcharts';
 
-var options = {
-    series: [
-        {
-            name: 'Desktops',
-            data: [10, 41, 35, 51, 49, 62, 69, 91, 148],
+const renderChart = () => {
+    const options = {
+        series: [
+            {
+                name: 'Desktops',
+                data: [10, 41, 35, 51, 49, 62, 69, 91, 148],
+            },
+        ],
+        chart: {
+            height: 350,
+            type: 'line',
+            zoom: {
+                enabled: false,
+            },
         },
-    ],
-    chart: {
-        height: 350,
-        type: 'line',
-        zoom: {
+        dataLabels: {
             enabled: false,
         },
-    },
-    dataLabels: {
-        enabled: false,
-    },
-    stroke: {
-        curve: 'straight',
-    },
-    title: {
-        text: 'Product Trends by Month',
-        align: 'left',
-    },
-    grid: {
-        row: {
-            colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
-            opacity: 0.5,
+        stroke: {
+            curve: 'straight',
         },
-    },
-    xaxis: {
-        categories: [
-            'Jan',
-            'Feb',
-            'Mar',
-            'Apr',
-            'May',
-            'Jun',
-            'Jul',
-            'Aug',
-            'Sep',
-        ],
-    },
+        title: {
+            text: 'Product Trends by Month',
+            align: 'left',
+        },
+        grid: {
+            row: {
+                colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+                opacity: 0.5,
+            },
+        },
+        xaxis: {
+            categories: [
+                'Jan',
+                'Feb',
+                'Mar',
+                'Apr',
+                'May',
+                'Jun',
+                'Jul',
+                'Aug',
+                'Sep',
+            ],
+        },
+    };
+
+    const chart = new ApexCharts(document.querySelector('#chart'), options);
+    chart.render();
 };
 
-var chart = new ApexCharts(document.querySelector('#chart'), options);
-chart.render();
+renderChart();
